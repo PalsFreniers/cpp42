27268739c02fd8c593cac109213c8827ca6dd47a
Tidian Delage <tdelage@3A6.42angouleme.fr>
2024-06-22 20:15:53 +0200

    cpp02 ready to push

--- 02/ex03/main.cpp
+++ 02/ex03/main.cpp
@@ -8,7 +8,7 @@ int main() {
                 Point c = Point(3, 4);
 
                 Point pt = Point(2, 2);
-                std::cout << "the point {2; 2} is in triangle :[{1, 1}, {4, 1}, {3, 4}] : " << std::boolalpha << bsp(a, b, c, pt) << std::noboolalpha << std::endl;
+                std::cout << "is the point {2; 2} is in triangle :[{1, 1}, {4, 1}, {3, 4}] : " << std::boolalpha << bsp(a, b, c, pt) << std::noboolalpha << std::endl;
         }
         {
                 Point a = Point(1, 1);
@@ -16,7 +16,7 @@ int main() {
                 Point c = Point(3, 4);
 
                 Point pt = Point(2, 2.6f);
-                std::cout << "the point {2; 2.6} is in triangle :[{1, 1}, {4, 1}, {3, 4}] : " << std::boolalpha << bsp(a, b, c, pt) << std::noboolalpha << std::endl;
+                std::cout << "is the point {2; 2.6} is in triangle :[{1, 1}, {4, 1}, {3, 4}] : " << std::boolalpha << bsp(a, b, c, pt) << std::noboolalpha << std::endl;
         }
         {
                 Point a = Point(1, 1);
@@ -24,14 +24,14 @@ int main() {
                 Point c = Point(3, 4);
 
                 Point pt = Point(-2, 2);
-                std::cout << "the point {-2; 2} is in triangle :[{1, 1}, {4, 1}, {3, 4}] : " << std::boolalpha << bsp(a, b, c, pt) << std::noboolalpha << std::endl;
+                std::cout << "is the point {-2; 2} is in triangle :[{1, 1}, {4, 1}, {3, 4}] : " << std::boolalpha << bsp(a, b, c, pt) << std::noboolalpha << std::endl;
         }
         {
                 Point a = Point(1, 1);
                 Point b = Point(4, 1);
                 Point c = Point(3, 4);
 
-                Point pt = Point(3.5f, 1.5f);
-                std::cout << "the point {3.5; 1.5} is in triangle :[{1, 1}, {4, 1}, {3, 4}] : " << std::boolalpha << bsp(a, b, c, pt) << std::noboolalpha << std::endl;
+                Point pt = Point(1.4f, 1.6f);
+                std::cout << "is the point {3.5; 1.5} is in triangle :[{1, 1}, {4, 1}, {3, 4}] : " << std::boolalpha << bsp(a, b, c, pt) << std::noboolalpha << std::endl;
         }
 }
