aa8764a6d5ddf8331b6a87712ecc3c24ae11a163
Tidian Delage <tdelage@1A5.42angouleme.fr>
2025-07-11 06:11:56 +0200

    fixing weird 0 behaviour on space

--- 09/ex02/src/main.cpp
+++ 09/ex02/src/main.cpp
@@ -1,4 +1,40 @@
 #include "../inc/PmergeMe.h"
+#include <cctype>
+#include <algorithm>
+#include <functional>
+
+inline std::string &ltrim(std::string &s) {
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(),
+            std::not1(std::ptr_fun<int, int>(std::isspace))));
+    return s;
+}
+
+inline std::string &rtrim(std::string &s) {
+    s.erase(std::find_if(s.rbegin(), s.rend(),
+            std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
+    return s;
+}
+
+inline std::string &trim(std::string &s) {
+    return ltrim(rtrim(s));
+}
+
+std::string normalise(std::string str) {
+        std::string ret;
+        bool norm = false;
+        for(__auto_type it = str.begin(); it != str.end(); it++) {
+                if(!std::isspace(*it)) {
+                        norm = false;
+                        ret += (*it);
+                        continue;
+                }
+                if(norm) continue;
+                ret += ' ';
+                norm = true;
+        }
+        
+        return ret;
+}
 
 int main(int argc, char *argv[]) {
         PmergeMe sorter;
@@ -16,6 +52,7 @@ int main(int argc, char *argv[]) {
         }
         if (!hasNumber) return 1;
         try {
+                sequence = normalise(sequence);
                 sorter.run(sequence);
         } catch (std::exception &e) {
                 ERROR BRED AND e.what() AND CLR ENDL;
